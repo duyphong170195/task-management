f78332098cce8206ac2fd63f8f141fb98fa322c2
duyphong170195 <duyphong170195@gmail.com>
2026-04-01 17:56:06 +0700

    replace hardcode to env

---
+++ src/main/java/com/eight_seneca/task_management/config/OpenApiConfig.java
@@ -0,0 +1,27 @@
+package com.eight_seneca.task_management.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Value("${swagger.server}")
+    private String swaggerServer;
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("My API")
+                        .version("1.0")
+                        .description("Demo Swagger với Spring Boot 3"))
+                .servers(List.of(new Server().url(swaggerServer)));
+    }
+}
